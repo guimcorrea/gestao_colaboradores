8cbb08ff4d10196df54c9ef4ae2028b98d8f3101
guimcorrea <guilherme.menegatto.github@gmail.com>
2025-04-14 00:53:29 -0300

    Update views.py

--- app/views.py
+++ app/views.py
@@ -1,1 +1,18 @@
+from django.shortcuts import render, redirect
+from .models import Colaborador
+from .forms import ColaboradorForm
+
+def lista_colaboradores(request):
+    colaboradores = Colaborador.objects.all()
+    return render(request, 'colaboradores/lista.html', {'colaboradores': colaboradores})
+
+def novo_colaborador(request):
+    if request.method == 'POST':
+        form = ColaboradorForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_colaboradores')
+    else:
+        form = ColaboradorForm()
+    return render(request, 'colaboradores/novo.html', {'form': form})
 
