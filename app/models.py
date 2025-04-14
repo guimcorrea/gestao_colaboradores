3e08d4e53a6a3f8f03adf7e9870f0ad00c365b14
guimcorrea <guilherme.menegatto.github@gmail.com>
2025-04-14 00:52:55 -0300

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,1 +1,10 @@
+from django.db import models
+
+class Colaborador(models.Model):
+    nome = models.CharField(max_length=100)
+    cargo = models.CharField(max_length=50)
+    data_admissao = models.DateField()
+
+    def __str__(self):
+        return self.nome
 
