aa158ee9fe4f7b8fbe3725823a93f4e1c5feb58f
guimcorrea <guilherme.menegatto.github@gmail.com>
2025-04-14 00:52:32 -0300

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -1,1 +1,8 @@
+from django import forms
+from .models import Colaborador
+
+class ColaboradorForm(forms.ModelForm):
+    class Meta:
+        model = Colaborador
+        fields = ['nome', 'cargo', 'data_admissao']
 
