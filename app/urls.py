1cc993ef6c9aba0f37c1bb9536705462358de53e
guimcorrea <guilherme.menegatto.github@gmail.com>
2025-04-14 00:53:13 -0300

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -1,1 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.lista_colaboradores, name='lista_colaboradores'),
+    path('novo/', views.novo_colaborador, name='novo_colaborador'),
+]
 
